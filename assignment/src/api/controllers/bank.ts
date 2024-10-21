38fbd306aee4dc78373128f2f3b6472905e335a0
KausTarafdar <kaustarafdar@gmail.com>
2024-10-21 13:58:44 +0530

    Add controllers

--- assignment/src/api/controllers/bank.ts
+++ assignment/src/api/controllers/bank.ts
@@ -1,9 +1,94 @@
 import { Request, Response } from "express";
 
-export function handleLending(req: Request, res: Response) {
+import { config } from "../../config/config";
+import { BankRepository } from "../../models";
+import { Banking_services } from "../../services/banking_services";
 
+const bankRepository: BankRepository = new BankRepository();
+const banking_services: Banking_services = new Banking_services(bankRepository);
+
+export function handleLending(req: Request, res: Response) {
+    try {
+        const { userId, loanAmount, period, interest } = req.body
+
+        if(
+            !userId ||
+            !loanAmount ||
+            !period ||
+            !interest
+        ) {
+            res.status(400).json({
+                status: config.ERROR,
+                message: "Request body incorrect",
+                data: {}
+            })
+            return
+        }
+
+        const getNewLoan = banking_services.lend(userId, loanAmount, period, interest);
+
+        res.status(200).json({
+            status: config.SUCCESS,
+            message: 'New user created',
+            data: getNewLoan
+        })
+        return
+
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({
+            status: config.ERROR,
+            message: "Something went wrong",
+            data: {}
+        })
+        return
+    }
 }
 
 export function handlePayment(req: Request, res: Response) {
+    try {
+        const { userId, loanId, amount, type } = req.body
 
+        if(
+            !userId ||
+            !loanId ||
+            !type
+        ) {
+            res.status(400).json({
+                status: config.ERROR,
+                message: "Request body incorrect",
+                data: {}
+            })
+            return
+        }
+
+        if(
+            type === config.LUMP_SUM && !amount
+        ) {
+            res.status(400).json({
+                status: config.ERROR,
+                message: "Specify amount for lumpsum payment",
+                data: {}
+            })
+            return
+        }
+
+        const makePayment = banking_services.payment(userId, loanId, amount, type);
+
+        res.status(200).json({
+            status: config.SUCCESS,
+            message: 'Payment successfully made',
+            data: makePayment
+        })
+        return
+
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({
+            status: config.ERROR,
+            message: "Something went wrong",
+            data: {}
+        })
+        return
+    }
 }
--- assignment/src/api/controllers/user.ts
+++ assignment/src/api/controllers/user.ts
@@ -1,13 +1,108 @@
 import { Request, Response} from "express";
 
-export function handleNewUser(req: Request, res: Response) {
+import { config } from "../../config/config";
+import { BankRepository } from "../../models";
+import { Banking_services } from "../../services/banking_services";
+
+const bankRepository: BankRepository = new BankRepository();
+const banking_services: Banking_services = new Banking_services(bankRepository);
+
+export async function handleNewUser(req: Request, res: Response) {
+    try {
+        const { username } = req.body
+
+        if(!username) {
+            res.status(400).json({
+                status: config.ERROR,
+                message: "No username provided",
+                data: {}
+            })
+            return
+        }
+
+        const createNewUser = await banking_services.addCustomer(username);
+
+        res.status(200).json({
+            status: config.SUCCESS,
+            message: 'New user created',
+            data: createNewUser
+        })
+        return
+
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({
+            status: config.ERROR,
+            message: "Something went wrong",
+            data: {}
+        })
+        return
+    }
 
 }
 
 export function handleGetLoans(req: Request, res: Response) {
+    try {
+        const { userId } = req.params
 
+        if(!userId) {
+            res.status(400).json({
+                status: config.ERROR,
+                message: "No user ID provided",
+                data: {}
+            })
+            return
+        }
+
+        const getLoans = banking_services.account_overview(userId);
+
+        res.status(200).json({
+            status: config.SUCCESS,
+            message: 'Retrieved all loans',
+            data: getLoans
+        })
+        return
+
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({
+            status: config.ERROR,
+            message: "Something went wrong",
+            data: {}
+        })
+        return
+    }
 }
 
 export function handleGetLedger(req: Request, res: Response) {
-    
+    try {
+        const { userId, loanId } = req.params
+
+        if(!userId || !loanId) {
+            res.status(400).json({
+                status: config.ERROR,
+                message: "No user Id or loan Id provided",
+                data: {}
+            })
+            return
+        }
+
+        const getLedger = banking_services.ledger(userId, loanId);
+
+        res.status(200).json({
+            status: config.SUCCESS,
+            message: 'Retrieved ledger for loan',
+            data: getLedger
+        })
+        return
+
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({
+            status: config.ERROR,
+            message: "Something went wrong",
+            data: {}
+        })
+        return
+    }
 }
